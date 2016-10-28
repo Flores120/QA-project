50b653e660789a265626a41b4eb03b1761e53248
Alvaro <alvaro.flores01@gmail.com>
2016-10-28 11:39:34 -0700

    fixed an issue with delete button in the question-details.hbs

--- app/routes/question.js
+++ app/routes/question.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
   return this.store.findRecord('question', params.question_id);
-}
+},
+actions:{
+  postQuestion(params){
+    var postQuestion = this.store.createRecord('question', params);
+    postQuestion.save();
+    this.transitionTo('index');
+  },
+    deleteQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    }
+  }
 });
