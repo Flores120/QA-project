002195a7240d57fdf501d34bec5f65cb54ec3227
Alvaro <alvaro.flores01@gmail.com>
2016-10-28 08:37:45 -0700

    this commit added data to the index.js

---
+++ app/routes/index.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+var questions = [{
+  author: "Angel",
+  question: "how musch does a dog cost?"
+}];
+
+export default Ember.Route.extend({
+  model(){
+    return questions;
+  }
+});
