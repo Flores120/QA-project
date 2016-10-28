b4c9245acd8411a6784745b389a8d032138dbf79
Alvaro <alvaro.flores01@gmail.com>
2016-10-28 13:17:22 -0700

    fixed the update button

--- app/routes/question.js
+++ app/routes/question.js
@@ -5,6 +5,16 @@ export default Ember.Route.extend({
   return this.store.findRecord('question', params.question_id);
 },
 actions:{
+  // update(question, params) {
+  //     Object.keys(params).forEach(function(key) {
+  //       if(params[key]!==undefined) {
+  //         question.set(key,params[key]);
+  //       }
+  //     });
+  //     question.save();
+  //     this.transitionTo('index');
+  //   },
+
   postQuestion(params){
     var postQuestion = this.store.createRecord('question', params);
     postQuestion.save();
