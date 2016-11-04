fa02084056aeb3591fac976e0d8dac5dd5754d18
Alvaro Flores <alvaro.flores01@gmail.com>
2016-11-04 13:00:47 -0700

    new if statement that shows if a question is answered or not

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -2,8 +2,10 @@ import Ember from 'ember';
 
 export function questionPopularity(params/*, hash*/) {
   var question = params[0];
-
-  if(question.get('answers').get('length') >= 5) {
+    if(question.get('answers').get('length') == 0) {
+      return Ember.String.htmlSafe('<span class="unansweredQ">Be the first to answer!</span><br>')
+    }
+    else if (question.get('answers').get('length') >= 5) {
     return Ember.String.htmlSafe('<img src="https://webappstatic.buzzfeed.com/static/images/global/og-image-trending.jpg"></>');
   }
 }
