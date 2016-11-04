1b660f571a72038dfbfad054462a23f42d4980af
Alvaro Flores <alvaro.flores01@gmail.com>
2016-11-04 09:19:30 -0700

    added a computed property

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  heading: Ember.computed('answer.author', 'answer.content', function() {
+    return this.get('answer.author') + ' - ' + this.get('answer.content');
+  }),
   actions:  {
     deleteQuestion(question){
       if (confirm('are you sure????')){
